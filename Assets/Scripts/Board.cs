296aa45c5f0cd83dc2cc50af7609d9e214780a6d
Russell Asher <russell.asher@igt.com>
2016-07-07 16:11:40 -0700

    Adding initial bombs unity project

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Board : MonoBehaviour {
+	public Transform piece;
+	private int xLength = 4;
+	private int yLength = 4;
+	private List<List<GameObject>> field = new List<List<GameObject>>(4);
+	// Use this for initialization
+	void Start () {
+		foreach( int xPos in Enumerable.Range( 0, xLength ) ){
+
+
+			List<GameObject> yList = new List<GameObject>();
+			int yDist = 0;
+			foreach( int yPos in Enumerable.Range(0, yLength)){
+
+				GameObject go;
+				go=Instantiate(piece.gameObject) as GameObject;
+				go.transform.position = new Vector3(xPos*60,yPos*60);
+				yList.Add(go);
+			}
+			field.Add(yList);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
---
+++ Assets/Scripts/Space.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Space : MonoBehaviour {
+
+	// Use this for initialization
+
+	Vector3 playerPos;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+	
+		Destroy( gameObject);
+
+	}
+
+	void NailedIt(){
+		print ("Rock on dude!");
+	}
+}
