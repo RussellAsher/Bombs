56525b628006bc0d8cb1b9ffe2fc1b6682cb69da
Russell Asher <RussellAsher@gmail.com>
2016-07-07 23:49:48 -0700

    made peices children of gameboard

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,32 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Board : MonoBehaviour {
+public class Board : MonoBehaviour
+{
 	public Transform piece;
 	private int xLength = 4;
 	private int yLength = 4;
-	private List<List<GameObject>> field = new List<List<GameObject>>(4);
-	// Use this for initialization
-	void Start () {
-		foreach( int xPos in Enumerable.Range( 0, xLength ) ){
+	private List<List<GameObject>> field = new List<List<GameObject>> (4);
 
+	void Start ()
+	{
+		foreach (int xPos in Enumerable.Range( 0, xLength )) {
 
-			List<GameObject> yList = new List<GameObject>();
+			List<GameObject> yList = new List<GameObject> ();
 			int yDist = 0;
-			foreach( int yPos in Enumerable.Range(0, yLength)){
+			foreach (int yPos in Enumerable.Range(0, yLength)) {
 
 				GameObject go;
-				go=Instantiate(piece.gameObject) as GameObject;
-				go.transform.position = new Vector3(xPos*60,yPos*60);
-				yList.Add(go);
+				go = Instantiate (piece.gameObject) as GameObject;
+				go.transform.parent = gameObject.transform;
+				go.transform.position = new Vector3 (xPos * 60, yPos * 60);
+				yList.Add (go);
 			}
-			field.Add(yList);
+			field.Add (yList);
 		}
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 	
 	}
